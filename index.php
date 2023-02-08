da19d884b0359bb78dc59e8ca7d348c3c2d7c568
Matteau-Pelletier <e2196113@cmaisonneuve.qc.ca>
2023-02-08 09:14:10 -0500

    c2 nouveau fichiers du theme

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<h1>Bonjour tout le monde, binvenue au cours 31W</h1>
+<?php
+/*
+    Modèle index.php représente le modèle par défaut du thème
+*/
+
+get_header(); ?>
+<main>
+    <h3>index.php</h3>
+</main>
+<?php
+if (have_posts()):
+    while (have_posts()): the_post();
+        the_title('<h1>','</h1>');
+    endwhile;
+endif;
+
+get_footer();
+?>
