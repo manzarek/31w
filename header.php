8d760a32ecea51a4f80e6d20548a593035c71006
Matteau-Pelletier <e2196113@cmaisonneuve.qc.ca>
2023-02-08 10:57:15 -0500

    c3 modification du header

--- header.php
+++ header.php
@@ -8,6 +8,10 @@
     <?php wp_head(); ?>
 </head>
 <body>
-    
+    <header>
+        <h1><?php bloginfo('name'); ?></h1>
+        <h2><?php bloginfo('description'); ?></h2>
+        <h2><?php bloginfo('url'); ?></h2>
+    </header>
 </body>
 </html>
