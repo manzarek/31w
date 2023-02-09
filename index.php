fca7370003eeaea381e4fdf2406fd1ea143a6e9b
Matteau-Pelletier <e2196113@cmaisonneuve.qc.ca>
2023-02-09 15:19:22 -0500

    c4 mise a jour du header

--- index.php
+++ index.php
@@ -6,13 +6,13 @@
 get_header(); ?>
 <main>
     <h3>index.php</h3>
-</main>
+
 <?php
 if (have_posts()):
     while (have_posts()): the_post();
         the_title('<h1>','</h1>');
     endwhile;
 endif;
-
-get_footer();
 ?>
+</main>
+<?php get_footer(); ?>
